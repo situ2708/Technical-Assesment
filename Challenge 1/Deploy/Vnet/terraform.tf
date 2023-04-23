5f0e78ba3bf76684f1a7aac03973c184948f9232
Akash Kaniti <akash.kaniti@pwc.com>
2023-04-23 21:39:00 +0530

    Code update

--- Challenge 1/Deploy/Vnet/terraform.tf
+++ Challenge 1/Deploy/Vnet/terraform.tf
@@ -6,11 +6,11 @@ module "vnet" {
   division            = "finance"
   service             = "vnet"
   vnet_name           = "vnet-finance-demo-eastus"
-  address_space       = ["10.1.0.0/20"]
+  address_space       = ["10.0.0.0/16"]
   subnets = {
-    "Web_subnet" = { range = "10.1.0.0/22", service_endpoints = [] }
-    "App_subnet" = { range = "10.1.4.0/23", service_endpoints = ["Microsoft.Sql"] }
-    "DB_subnet"  = { range = "10.1.6.0/23", service_endpoints = [] }
+    "Web_subnet" = { range = "10.0.1.0/24", service_endpoints = [] }
+    "App_subnet" = { range = "10.0.2.0/24", service_endpoints = ["Microsoft.Sql"] }
+    "DB_subnet"  = { range = "10.0.3.0/24", service_endpoints = [] }
   }
   network_security_groups = ["nsg-web", "nsg-app", "nsg-db"]
   nsg_rules_web = {
@@ -23,7 +23,7 @@ module "vnet" {
       source_port_range          = "80"
       destination_port_range     = "80"
       source_address_prefixes    = ["AzureLoadBalancer"]
-      destination_address_prefix = "10.1.0.0/22"
+      destination_address_prefix = "10.0.1.0/24"
     },
     "ssh" = {
       name                       = "ssh"
@@ -34,7 +34,7 @@ module "vnet" {
       source_port_range          = "*"
       destination_port_range     = "22"
       source_address_prefixes    = ["VirtualNetwork"]
-      destination_address_prefix = "10.1.0.0/22"
+      destination_address_prefix = "10.0.1.0/24"
     },
   }
 
@@ -47,8 +47,8 @@ module "vnet" {
       protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "8081"
-      source_address_prefixes    = ["10.1.0.0/22"]
-      destination_address_prefix = "10.1.4.0/23"
+      source_address_prefixes    = ["10.0.1.0/24"]
+      destination_address_prefix = "10.0.2.0/24"
     },
     "app-inbound-2" = {
       name                       = "app-inbound-2"
@@ -58,8 +58,19 @@ module "vnet" {
       protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "8080"
-      source_address_prefixes    = ["10.1.0.0/22"]
-      destination_address_prefix = "10.1.4.0/23"
+      source_address_prefixes    = ["10.0.1.0/24"]
+      destination_address_prefix = "10.0.2.0/24"
+    },
+    "app-ssh" = {
+      name                       = "app-vm-ssh"
+      priority                   = "204"
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefixes    = ["VirtualNetwork"]
+      destination_address_prefix = "10.0.2.0/24"
     }
   }
 
@@ -72,8 +83,8 @@ module "vnet" {
       protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "5432"
-      source_address_prefixes    = ["10.1.4.0/23"]
-      destination_address_prefix = "10.1.6.0/23"
+      source_address_prefixes    = ["10.0.2.0/24"]
+      destination_address_prefix = "10.0.3.0/24"
     }
   } 
     tags = {
